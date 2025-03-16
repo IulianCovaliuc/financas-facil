bfce04f908287aed1e2ce55981189cbc0452556b
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-03-16 23:43:09 +0000

    Account Editing and a few more things were added. Moving to Transaction and Categories API

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,42 +3,41 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
-import { columns, Payment } from "./columns";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
-
-async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      // ...
-    ]
-}
-
-const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-        id: "738ed52f",
-        amount: 1200,
-        status: "pending",
-        email: "22m@example.com",
-      },
-    // ...
-]
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 
 const AccountsPage = () => {
+
     const newAccount = useNewAccount();
+    const deleteAccounts = useBulkDeleteAccounts();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    const isDisabled =
+        accountsQuery.isLoading || deleteAccounts.isPending;
+
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48"/> 
+                    </CardHeader>
+                    <CardContent>
+                        <div className="5-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -52,7 +51,15 @@ const AccountsPage = () => {
                     </Button>
                 </CardHeader>
                 <CardContent>
-                    <DataTable filterKey="email" columns={columns} data={data} />
+                    <DataTable 
+                    filterKey="email" 
+                    columns={columns} 
+                    data={accounts} 
+                    onDelete={(row) => {
+                        const ids = row.map((r) => r.original.id);
+                        deleteAccounts.mutate({ ids });
+                    }} 
+                    disabled={isDisabled}/>
                 </CardContent>
             </Card>
         </div>
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -5,6 +5,7 @@ import * as React from "react"
 import {
   ColumnDef,
   ColumnFiltersState,
+  Row,
   SortingState,
   flexRender,
   getCoreRowModel,
@@ -23,20 +24,33 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { useConfirm } from "@/hooks/use-confirm"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Trash } from "lucide-react"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
   filterKey: string
+  onDelete: (rows: Row<TData>[]) => void;
+  disabled?: boolean;
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
   filterKey,
+  onDelete,
+  disabled,
 }: DataTableProps<TData, TValue>) {
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure ?",
+    "You are about to delete multiple items."
+  )
+
 
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
@@ -63,6 +77,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+        <ConfirmDialog />
         <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -72,6 +87,25 @@ export function DataTable<TData, TValue>({
           }
           className="max-w-sm"
         />
+        {table.getFilteredSelectedRowModel().rows.length > 0 && (
+            <Button
+                disabled={disabled} 
+                size="sm"
+                variant="outline"
+                className="ml-auto font-normal text-xs"
+                onClick={async () => {
+                  const ok = await confirm();
+
+                  if (ok) {
+                    onDelete(table.getFilteredSelectedRowModel().rows)
+                    table.resetRowSelection();
+                  }
+                }}
+            >
+                <Trash className="size-4 mr-2" />
+                Delete ({table.getFilteredSelectedRowModel().rows.length})
+            </Button>
+        )}
         </div>
         <div className="rounded-md border">
         <Table>
@@ -143,3 +177,4 @@ export function DataTable<TData, TValue>({
     </div>
   )
 }
+
