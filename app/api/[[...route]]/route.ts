8c3f93fad64d50a50a48b42e00873e6b36033cb2
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-03-22 21:59:52 +0000

    Transactions Hook + Api Added

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
 import { HTTPException } from 'hono/http-exception'
+import categories from './categories'
+import transactions from './transactions'
 
 
 export const runtime = 'edge'
@@ -9,7 +11,9 @@ export const runtime = 'edge'
 const app = new Hono().basePath('/api')
 
 const routes = app
-    .route("/accounts", accounts);
+    .route("/accounts", accounts)
+    .route("/categories", categories)
+    .route("transactions", transactions);
 
 export const GET = handle(app)
 export const POST = handle(app)
