c5c68af02f56b3bd41ac3e16ba3a4a31f8963d72
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-03-25 19:28:06 +0000

    Added possibility to add Income and Expense by providing + or - symbols. Need to change these to better alternatives.

--- components/amount-input.tsx
+++ components/amount-input.tsx
@@ -52,6 +52,24 @@ export const AmountInput = ({
           <TooltipContent>
             Use [+] for Income and [-] for expenses
           </TooltipContent>
+          <CurrencyInput
+            prefix="$"
+            className={cn(
+              "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              "pl-10 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+              "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+            )}
+            placeholder={placeholder}
+            value={value}
+            decimalsLimit={2}
+            decimalScale={2}
+            onValueChange={onChange}
+            disabled={disabled}
+          />
+          <p className="text-xs text-muted-foreground mt-2">
+            {isIncome && "This will count as income"}
+            {isExpense && "This will count as expense"}
+          </p>
         </Tooltip>
       </TooltipProvider>
     </div>
