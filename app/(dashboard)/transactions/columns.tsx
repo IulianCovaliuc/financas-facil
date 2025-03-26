fa34efbb6d955bccbd5f1f41bce1b30f0ae66851
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-03-26 23:08:16 +0000

    Added Category, Payee and AMount fields to the transaction Page, added values, need to Fix Value Bugs

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -9,6 +9,7 @@ import { client } from "@/lib/hono";
 import { ArrowUpDown } from "lucide-react";
 import { Actions } from "./actions";
 import { format } from "date-fns";
+import { formatCurrency } from "@/lib/utils";
 
 export type ResponseType = InferResponseType<
   typeof client.api.transactions.$get,
@@ -56,6 +57,56 @@ export const columns: ColumnDef<ResponseType>[] = [
       const date = row.getValue("date") as Date;
 
       return <span>{format(date, "dd MMMM, yyyy")}</span>;
+    },
+  },
+  {
+    accessorKey: "category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Category
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <span>{row.original.category}</span>;
+    },
+  },
+  {
+    accessorKey: "payee",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Payee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      return <span>{formatCurrency(amount)}</span>;
     },
   },
   {
