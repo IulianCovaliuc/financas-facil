f97ccfe0b552ccbd7791c8de1e761cb4218552fd
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-04-04 23:54:09 +0100

    Starting to work on Dashboard Page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,14 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
-export default function Home() {
-  const { onOpen } = useNewAccount();
-
-  return (
-    <div>
-      <Button onClick={onOpen}>
-        Add and account
-      </Button>
-    </div>
-  )
-};
+export default function DashBoardPage() {
+  return <div className=""></div>;
+}
