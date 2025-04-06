0d0bdea1086bb5abc86a222b6350125ce1ee5cdf
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-04-06 02:05:23 +0100

    Added Dashboards for Transactions

--- lib/utils.ts
+++ lib/utils.ts
@@ -86,3 +86,20 @@ export function formatDateRange(period?: Period) {
   }
   return format(period.from, "LLL dd, y");
 }
+
+export function formatPercentage(
+  value: number,
+  options: { addPrefix?: boolean } = {
+    addPrefix: false,
+  }
+) {
+  const result = new Intl.NumberFormat("eb-US", {
+    style: "percent",
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+
+  return result;
+}
