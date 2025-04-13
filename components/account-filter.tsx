3e7b095f4f4123ed783a613b8dc5d81d7cb75fd6
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-04-13 17:38:16 +0100

    Apply button requires some fixing, otherwise things seems to be rather fine. Need to upgrade and update app properly for better functions

--- components/account-filter.tsx
+++ components/account-filter.tsx
@@ -5,6 +5,7 @@ import qs from "query-string";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
 
 import {
   Select,
@@ -22,6 +23,8 @@ export const AccountFilter = () => {
   const accountId = params.get("accountId") || "all";
   const from = params.get("from") || "";
   const to = params.get("to") || "";
+
+  const { isLoading: isLoadingSummary } = useGetSummary();
 
   const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
 
@@ -50,7 +53,7 @@ export const AccountFilter = () => {
     <Select
       value={accountId}
       onValueChange={onChange}
-      disabled={isLoadingAccounts}
+      disabled={isLoadingAccounts || isLoadingSummary}
     >
       <SelectTrigger
         className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent
