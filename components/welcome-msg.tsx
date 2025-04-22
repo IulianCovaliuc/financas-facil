fbe7a474571f4cb7ae64fd4ce327e995542fcd0f
Iulian <93096007+IulianCovaliuc@users.noreply.github.com>
2025-04-22 23:20:33 +0100

    Keeping Indentations and things clean. Changed simple text color

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -3,16 +3,16 @@
 import { useUser } from "@clerk/nextjs";
 
 export const WelcomeMsg = () => {
-    const { user, isLoaded } = useUser();
+  const { user, isLoaded } = useUser();
 
-    return (
-        <div className="space-y-2 mb-4">
-            <h2 className="text-2xl lg:text-4xl text-white font-medium">
-                Bem vindo de Volta {isLoaded ? ", " : " "} {user?.firstName} 
-            </h2>
-            <p className="text-sm lg:text-base text-[#89b6fd]">
-                Este é o teu relatório geral
-            </p>
-        </div>
-    );
+  return (
+    <div className="space-y-2 mb-4">
+      <h2 className="text-2xl lg:text-4xl text-white font-medium">
+        Bem vindo de Volta {isLoaded ? ", " : " "} {user?.firstName}
+      </h2>
+      <p className="text-sm lg:text-base text-[#000a18]">
+        Este é o teu relatório geral
+      </p>
+    </div>
+  );
 };
